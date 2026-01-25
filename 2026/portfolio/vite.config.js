a3c5707aa5a0a8c8fa111b25c01edf5c3e81e305
n-is-k <57360746+n-is-k@users.noreply.github.com>
2026-01-26 00:29:07 +0100

    Fix Vite base path for custom domain

--- 2026/portfolio/vite.config.js
+++ 2026/portfolio/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() ,   tailwindcss(),],
-      base: '/Kirtis-Clarke-2026/',
+      base: '/',
 
 
 })
